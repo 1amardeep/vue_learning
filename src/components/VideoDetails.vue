d5849b7237e9497d62f30450736a9248fbbed0c9
1amardeep <amardeepsinghgahlot@gmail.com>
2021-08-24 13:50:29 +0530

    iframe push

--- src/components/VideoDetails.vue
+++ src/components/VideoDetails.vue
@@ -2,6 +2,9 @@
 <div>
  <h1>Details about the course : {{videoDetails.title}}</h1>   
  <hr>
+ <iframe width="600" height="400" title="course"
+  v-bind:src="`https://www.youtube.com/embed/${videoDetails.src}`">
+</iframe>
 <p>{{videoDetails.discription}}</p>
   </div>
   
